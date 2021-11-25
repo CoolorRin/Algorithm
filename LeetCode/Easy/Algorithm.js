f2df9d546d7aee7ac758faafd52e1904b1217848
AyatukiRin <hosibosi@pop-os.localdomain>
2021-11-25 22:36:04 +0800

    [Daily Commit] LeetCode Algorithm Exercise Esay Queation: Roman2Integer.

--- LeetCode/Easy/Algorithm.js
+++ LeetCode/Easy/Algorithm.js
@@ -35,6 +35,31 @@ class Solution {
 
     };
 
+    transferRoman2Integer(romanNumString) {
+        const RomanMap = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+        strLength = romanNumString.length
+        let sum = RomanMap[romanNumString[strLength - 1]]
+        for (const index = strLength - 2; index >= 0; index--) {
+            if (RomanMap[romanNumString[index]] < RomanMap[romanNumString[index + 1]]) {
+                sum -= RomanMap[romanNumString[index]]
+            }
+            else {
+                sum += RomanMap[romanNumString[index]]
+            }
+        }
+        return sum
+    }
+
 }
 // Test 
 const testSolution = new Solution()
+const result = testSolution.transferRoman2Integer("III")
+console.log(result);
