78244a0ed5455f59d67d9fcb1256327e8dae9255
AyatukiRin <AyatukiRin@gmail.com>
2022-09-23 09:24:17 +0800

    [Daily Commit] LeetCode Algorithm Exercise: (219) Contains Duplicate II.

--- LeetCode/Medium/Algorithm.js
+++ LeetCode/Medium/Algorithm.js
@@ -89,41 +89,43 @@ class solutions {
 
 	addTwoNumber = (l1, l2) => {
 		// Wrong Answer. Javascript have max safe integer: Number.MAX_SAFE_INTEGER
-		// const convertArr2LinkedList = (arr) => {
-		// 	const result = new ListNode();
-		// 	let currNode = result;
-		// 	arr.forEach((el, index) => {
-		// 		currNode.val = el;
-		// 		if (index !== arr.length - 1) {
-		// 			currNode.next = new ListNode();
-		// 			currNode = currNode.next;
-		// 		}
-		// 	});
-		// 	return result;
-		// };
-		// const convertNum2Arr = (num) => {
-		// 	const result = [];
-		// 	while (num > 0) {
-		// 		result.push(num % 10);
-		// 		num = Math.floor(num / 10);
-		// 	}
-		// 	return result;
-		// };
-		// const convert2Number = (listNode, exponent) => {
-		// 	let sum = 0;
-		// 	if (listNode.next) {
-		// 		const result = convert2Number(listNode.next, exponent + 1);
-		// 		sum += result;
-		// 	} else return listNode.val * Math.pow(10, exponent);
-		// 	sum += listNode.val * Math.pow(10, exponent);
-		// 	return sum;
-		// };
-		// const l1_num = convert2Number(l1, 0);
-		// const l2_num = convert2Number(l2, 0);
-		// console.log(l1_num);
-		// const targetNum_arr = convertNum2Arr(l1_num + l2_num);
-		// const result = convertArr2LinkedList(targetNum_arr);
-		// return result;
+		const convertArr2LinkedList = (arr) => {
+			const result = new ListNode();
+			let currNode = result;
+			arr.forEach((el, index) => {
+				currNode.val = el;
+				if (index !== arr.length - 1) {
+					currNode.next = new ListNode();
+					currNode = currNode.next;
+				}
+			});
+			return result;
+		};
+		const convertNum2Arr = (num) => {
+			const result = [];
+			while (num > 0) {
+				result.push(num % 10);
+				num = Math.floor(num / 10);
+			}
+			return result;
+		};
+		const convert2Number = (listNode, exponent) => {
+			let sum = 0;
+			if (listNode.next) {
+				const result = convert2Number(listNode.next, exponent + 1);
+				sum += result;
+			} else return listNode.val * Math.pow(10, exponent);
+			sum += listNode.val * Math.pow(10, exponent);
+			return sum;
+		};
+		const l1_num = convert2Number(l1, 0);
+		const l2_num = convert2Number(l2, 0);
+		console.log(l1_num);
+		const targetNum_arr = convertNum2Arr(l1_num + l2_num);
+		const result = convertArr2LinkedList(targetNum_arr);
+		return result;
+
+		//
 	};
 }
 
