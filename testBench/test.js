762d5db181b7ab15542951c1a296433e7632432e
AyatukiRin <AyatukiRin@gmail.com>
2022-10-13 16:23:10 +0800

    [Temp Testbench]

--- testBench/test.js
+++ testBench/test.js
@@ -1,22 +1,21 @@
-function TreeNode(val, left, right) {
-	this.val = val === undefined ? 0 : val;
-	this.left = left === undefined ? null : left;
-	this.right = right === undefined ? null : right;
-}
-
-const invertTree = (root) => {
-	if (root === undefined) return;
-	const tmp = root.right;
-	root.right = root.left;
-	root.left = tmp;
-	invertTree(root.left);
-	invertTree(root.right);
+const summaryRanges = (nums) => {
+	const resultArr = new Array();
+	let startNum = nums[0];
+	let endNum = nums[0];
+	for (let index = 1; index <= nums.length; index++) {
+		if (nums[index] != endNum + 1) {
+			if (startNum === endNum) {
+				resultArr.push(startNum.toString());
+			} else {
+				resultArr.push(`${startNum}->${endNum}`);
+			}
+			startNum = nums[index];
+			endNum = nums[index];
+		} else {
+			endNum = nums[index];
+		}
+	}
+	return resultArr;
 };
 
-const test = (el) => {
-	if (el === 3) return el;
-	console.log("test");
-	test(el + 1);
-	return el;
-};
-console.log(test(1));
+console.log(summaryRanges([0, 2, 3, 4, 6, 8, 9]));
