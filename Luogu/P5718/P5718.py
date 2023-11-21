1a5d712f81849a9b286d0ef52b9615fcd9bdee22
MorkRin <MorkRinMi@gmail.com>
2023-11-21 14:48:45 +0800

    🦄 refactor(Algorithm Site Delete): Folder Delete
    Delete the algorithm practice of the Luogu&Niuke site.

--- Luogu/P5718/P5718.py
+++
@@ -1,4 +0,0 @@
-loop_time = int(input())
-# print(min(list(map(int, input().split()))))
-lt = list(map(int, input().split()))
-print(min(lt))
--- testBench/test.js
+++
@@ -1,7 +0,0 @@
-const isUgly = (num) => {
-	if (num === 0) return false;
-	if (num % 2 === 0) return isUgly(num / 2);
-	else if (num % 3 === 0) return isUgly(num / 3);
-	else if (num % 5 === 0) return isUgly(num / 5);
-	return num === 1;
-};
